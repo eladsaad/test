31c7df6b0021834d85958fdaf900c258f6eebd45
Dor Itzhaki <dor.itzhaki@gmail.com>
2013-12-05 12:59:49 +0000

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cinemadrive::Application.config.secret_key_base = 'fd5a4528ee4a458c50c452a407f70552a2931a97317d517d28622556e9e941dbb74957d7a17ad55bfad635545c0f9bb2d859f948ae3c47191c5ae2eaca999460'
