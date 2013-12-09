6f815bbd33206f0e2bd3ee64402d01d1303c6f80
Dor Itzhaki <dor.itzhaki@gmail.com>
2013-12-09 19:43:25 +0000

    fix for player registration controller

--- app/controllers/players/registrations_controller.rb
+++ app/controllers/players/registrations_controller.rb
@@ -1,2 +1,11 @@
 class Players::RegistrationsController < Devise::RegistrationsController
+	
+	before_filter :configure_permitted_parameters
+
+  	protected
+
+	  	def configure_permitted_parameters
+		    devise_parameter_sanitizer.for(:sign_up) << [:username, :email, :first_name, :last_name, :birth_date]
+		    devise_parameter_sanitizer.for(:account_update) << [:username, :email, :first_name, :last_name, :birth_date]
+	  	end
 end
