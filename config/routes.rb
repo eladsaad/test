be38b6c1087d7a43743612c061607bdc9ebc92ef
Dor Itzhaki <dor.itzhaki@gmail.com>
2013-12-09 17:07:45 +0000

    initial devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Cinemadrive::Application.routes.draw do
 
+  root :to => "players#index"
   resources :players
 
   # /admin - system administration
   namespace :admin do
+    root :to => "operators#index"
     resources :system_admins
     resources :operators
   end
 
   # /operation - operators administration
   namespace :operation do
+    root :to => "player_groups#index"
     resources :player_organizations
     resources :player_groups
   end
