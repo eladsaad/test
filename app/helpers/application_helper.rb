f1ef89a5990bf29fe25d695f9663d64942d9e7f6
Dor Itzhaki <dor.itzhaki@gmail.com>
2013-12-09 17:06:30 +0000

    added flash messages to layouts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def flash_class(level)
+		case level
+			when :notice then "alert-msg-notice"
+			when :success then "alert-msg-success"
+			when :error then "alert-msg-error"
+			when :alert then "alert-msg-alert"
+			else level.to_s
+		end
+	end
+
 end
