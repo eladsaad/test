b0c4a639b9e5e046149c6408a0bf3d84798656ed
Dor Itzhaki <dor.itzhaki@gmail.com>
2013-12-16 11:22:45 +0000

    proper redirect after signout

--- app/controllers/admin/system_admins/sessions_controller.rb
+++ app/controllers/admin/system_admins/sessions_controller.rb
@@ -1,3 +1,7 @@
 class Admin::SystemAdmins::SessionsController < Devise::SessionsController
 	layout 'admin/layouts/admin'
+
+	def after_sign_out_path_for(system_admin_symbol)
+		admin_root_url
+	end
 end
--- app/controllers/operation/operators/sessions_controller.rb
+++ app/controllers/operation/operators/sessions_controller.rb
@@ -1,3 +1,7 @@
 class Operation::Operators::SessionsController < Devise::SessionsController
 	layout 'operation/layouts/operation'
+
+	def after_sign_out_path_for(operator_symbol)
+		operation_root_url
+	end
 end
