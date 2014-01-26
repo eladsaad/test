ec8613f275ec2690a79400157b2918646e6592a7
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-01-26 11:42:39 +0000

    minor change

--- app/models/registration_code.rb
+++ app/models/registration_code.rb
@@ -1,7 +1,6 @@
 class RegistrationCode < ActiveRecord::Base
 
-	def self.fetch(amount, greater_than_id)
-		greater_than_id = 0 if greater_than_id.nil?
+	def self.fetch(amount, greater_than_id = 0)
 		RegistrationCode.where("id > ?", greater_than_id).order(:id).limit(amount)
 	end
 
