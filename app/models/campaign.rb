70ce23c6ba220e1ebd9570acc0b8cfe51a5e0ddd
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-10 11:35:24 +0000

    added campaigns click and show content funcionality

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -11,4 +11,22 @@ class Campaign < ActiveRecord::Base
 	# == SEARCH ==
 	search_columns [:name]
 
+	# == METHODS ==
+
+	def self.get_by_player_group(player_group)
+		self.where(
+			operator_id: player_group.operator_id,
+			online_program_id: player_group.online_program_id
+		).where(
+			"views < max_views"
+		).first
+	end
+
+	def show_banner_content(banner_number)
+		content = self.send("banner_html_#{banner_number}")
+		self.views += 1
+		self.save!
+		return content
+	end
+
 end
