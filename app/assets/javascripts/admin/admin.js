5f02f357f7e04bcd3fc55fd4fe3d7da598740d71
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-11 15:38:24 +0000

    seperated admin assets from main site

---
+++ app/assets/javascripts/admin/admin.js
@@ -0,0 +1,10 @@
+//
+// Javascript manifest for admin and operation namespaces
+//
+//= require jquery
+//= require jquery_ujs
+//= require jquery.ui.autocomplete
+//= require jquery.ui.datepicker
+//= require jquery_nested_form
+//= require bootstrap.min
+//= require_tree .
