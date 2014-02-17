cd6689f460c6e55b75cf0655fc0eb35e374756a3
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-17 14:38:35 +0000

    fixed problem with first group creation

--- app/models/registration_code.rb
+++ app/models/registration_code.rb
@@ -1,6 +1,7 @@
 class RegistrationCode < ActiveRecord::Base
 
 	def self.fetch(amount, greater_than_id = 0)
+		greater_than_id = 0 if greater_than_id.blank?
 		RegistrationCode.where("id > ?", greater_than_id).order(:id).limit(amount)
 	end
 
