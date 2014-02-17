8e225491a61a4e5198fe8bd3cbf4dca7a925f906
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-17 20:57:59 +0000

    after confirmation redirect

--- app/controllers/admin/system_admins/confirmations_controller.rb
+++ app/controllers/admin/system_admins/confirmations_controller.rb
@@ -1,3 +1,7 @@
 class Admin::SystemAdmins::ConfirmationsController < Devise::ConfirmationsController
 	layout 'admin/layouts/admin'
+
+	def after_confirmation_path_for(resource_name, resource)
+    	admin_root_url
+  	end
 end
--- app/controllers/operation/operators/confirmations_controller.rb
+++ app/controllers/operation/operators/confirmations_controller.rb
@@ -1,3 +1,7 @@
 class Operation::Operators::ConfirmationsController < Devise::ConfirmationsController
 	layout 'operation/layouts/operation'
+
+	def after_confirmation_path_for(resource_name, resource)
+    	admin_root_url
+  	end
 end
