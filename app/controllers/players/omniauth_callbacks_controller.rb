c65d6de3965f5a90834594774366b3f74b9379ab
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-17 09:20:51 +0000

    keep only required facebook data in session

--- app/controllers/players/omniauth_callbacks_controller.rb
+++ app/controllers/players/omniauth_callbacks_controller.rb
@@ -9,7 +9,18 @@ class Players::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 			set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
 			redirect_to player_dashboard_path
 		else
-			session["devise.facebook_data"] = request.env["omniauth.auth"]
+			session["devise.facebook_data"] ||= {}
+			session["devise.facebook_data"]["info"] ||= {}
+			session["devise.facebook_data"]["extra"] ||= {}
+			session["devise.facebook_data"]["extra"]["raw_info"] ||= {}
+			session["devise.facebook_data"]["info"]["image"] = request.env["omniauth.auth"]["info"]["image"]
+			session["devise.facebook_data"]["extra"]["raw_info"]["email"] = request.env["omniauth.auth"]["extra"]["raw_info"]["email"]
+			session["devise.facebook_data"]["extra"]["raw_info"]["first_name"] = request.env["omniauth.auth"]["extra"]["raw_info"]["first_name"]
+			session["devise.facebook_data"]["extra"]["raw_info"]["last_name"] = request.env["omniauth.auth"]["extra"]["raw_info"]["last_name"]
+			session["devise.facebook_data"]["extra"]["raw_info"]["gender"] = request.env["omniauth.auth"]["extra"]["raw_info"]["gender"]
+			session["devise.facebook_data"]["extra"]["raw_info"]["birthday"] = request.env["omniauth.auth"]["extra"]["raw_info"]["birthday"]
+
+			# session["devise.facebook_data"] = request.env["omniauth.auth"]
 			redirect_to new_player_registration_path(reg_code: request.env["omniauth.params"]['reg_code'])
 		end
 	end
