83767571aefa1b0c9879be4b163402801852f36d
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-02-17 15:18:42 +0000

    added interactive_video#show page

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,11 @@ Cinemadrive::Application.routes.draw do
   
   get '/dashboard' => "players#dashboard", :as => :player_dashboard
   get '/campaigns/click/:id' => "campaigns#click", :as => :click_campaign
-  resources :interactive_videos, only: [:index, :show]
+  resources :interactive_videos, only: [:index, :show] do
+    member do
+      get :content
+    end
+  end
   root :to => "static_pages#welcome"
     
 
