c378d530f9d6c35d00d6b39d69f32788ef5454f2
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-03-17 14:35:01 +0000

    added post route for facebook canvas page

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Cinemadrive::Application.routes.draw do
   root :to => "static_pages#welcome"
   get '/about' => "static_pages#about"
   get '/facebook_canvas_page' => "static_pages#facebook_canvas_page"
+  put '/facebook_canvas_page' => "static_pages#facebook_canvas_page"
   get '/scores' => "scores#index"
   get '/accept_tos' => 'players#edit_accept_tos', :as => "edit_accept_tos_player"
   put '/accept_tos' => 'players#update_accept_tos', :as => "update_accept_tos_player"  
