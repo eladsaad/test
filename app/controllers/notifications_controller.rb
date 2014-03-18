57aaa052d5bbfb888e9b595966b451609b292483
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-03-18 10:38:42 +0000

    debug facebook params on heroku

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,8 @@
 class NotificationsController < BaseController
 
 	skip_before_filter :verify_authenticity_token, only: [:facebook]
+	skip_before_filter :authenticate_player!, only: [:facebook]
+	skip_before_filter :tos_accepted, only: [:facebook]
 
 	def facebook
 		@notification = Notification.find(params[:id])
