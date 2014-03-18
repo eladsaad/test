2ede403d586bc3055a6e56ae44f24c65a027bd4a
Dor Itzhaki <dor.itzhaki@gmail.com>
2014-03-18 10:40:25 +0000

    debug facebook params on heroku

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,9 +5,9 @@ class NotificationsController < BaseController
 	skip_before_filter :tos_accepted, only: [:facebook]
 
 	def facebook
-		@notification = Notification.find(params[:id])
-		authorize! :read, @notification
-		@notification_content = Notification.parse_text(@notification.facebook_content, current_player)
+		# @notification = Notification.find(params[:id])
+		# authorize! :read, @notification
+		# @notification_content = Notification.parse_text(@notification.facebook_content, current_player)
 		render :layout => false
 	end
 
