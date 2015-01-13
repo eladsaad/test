582d22b4c26d97749c6072a9789677c4b2364f23
purian <purian@gmail.com>
2015-01-13 16:51:08 +0200

    Remove the conversion from http to https

--- app/models/jobs/DownloadFacebookImageJob.rb
+++ app/models/jobs/DownloadFacebookImageJob.rb
@@ -9,7 +9,7 @@ class DownloadFacebookImageJob
 
 	def perform
 		Rails.logger.debug "Download image for player id: #{@player.id} , image url: #{@image_url}"
-    @player.avatar = open(@image_url.gsub('http','https')+"?type=square")
+    @player.avatar = open(@image_url+"?type=square")
     @player.save
 	end
 
